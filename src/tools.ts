fa2e8f7f216be95f5b7000948dc9e763c963101d
Matt Collins <matt@mattcollins.net>
2025-11-18 14:40:04 +0000

    feat: add canned weather tool

--- src/tools.ts
+++ src/tools.ts
@@ -1,1 +1,16 @@
-export const tools = {};
+import { tool, jsonSchema } from 'ai';
+
+export const tools = {
+  getWeather: tool({
+    description: 'Returns a silly canned weather string.',
+    inputSchema: jsonSchema({
+      type: 'object',
+      properties: {},
+      additionalProperties: false
+    }),
+    outputSchema: jsonSchema({
+      type: 'string'
+    }),
+    execute: () => "It's cold. Brrrrrrrr!"
+  })
+};
